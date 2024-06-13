a985c631a557c5885da0ab9b4e910151d1190108
IvanGorbunov <gezer.ivan@yandex.ru>
2024-06-13 21:35:29 +0300

    add 6 HW

--- lesson_5/Firefox/Модальное окно.py
+++ lesson_5/Firefox/Модальное окно.py
@@ -1,11 +1,10 @@
 from time import sleep
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
 driver.get("http://the-internet.herokuapp.com/entry_ad")
 sleep(5)
